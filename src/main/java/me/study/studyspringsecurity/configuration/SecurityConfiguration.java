408a3458327df45d38b48ae29e925cef9d2e1c89
yoonjeong <cyj199637@gmail.com>
2021-05-05 21:34:01 +0900

    210505| Section 0

--- src/main/java/me/study/studyspringsecurity/configuration/SecurityConfiguration.java
+++ src/main/java/me/study/studyspringsecurity/configuration/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package me.study.studyspringsecurity.configuration;
 
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
