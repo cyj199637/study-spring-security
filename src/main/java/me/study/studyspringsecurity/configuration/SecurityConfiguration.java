09b9442d086b8dab0bfc80da0ae0854792c243d2
cyj199637 <cyj199637@gmail.com>
2021-05-05 21:22:51 +0900

    210505 | Section 0

---
+++ src/main/java/me/study/studyspringsecurity/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package me.study.studyspringsecurity.configuration;
+
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        httpSecurity.formLogin();
+        httpSecurity.httpBasic();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("yoonjeong").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}456").roles("ADMIN");
+    }
+}
---
+++ src/test/java/me/study/studyspringsecurity/StudySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.studyspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
